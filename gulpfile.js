1087b6950e8f0fd243853f8b62989f7e423470b6
Frank Baele <frank@localhost.localdomain>
2016-03-02 13:26:59 +0100

    basic setup connnect app

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp = require('gulp');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var notifier = require('node-notifier');
+var util = require('gulp-util');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var inlinesource = require('gulp-inline-source');
+
+gulp.task('inlinesource', function () {
+    return gulp.src('./index.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browserify', function () {
+    return build('main', 'bundle');
+});
+
+function build() {
+    var bundler = browserify('./src/index.js', {fullPaths: false});
+    bundler.on('log', util.log.bind(util));
+    bundler.bundle()
+        .on('error', util.log)
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./src'));
+}
+
+gulp.task('watchify', function () {
+    return bundle('main', 'bundle')
+});
+
+function bundle() {
+    watchify.args.debug = true;
+    watchify.args.fullPaths = true;
+    var bundler = watchify(browserify('./src/index.js', watchify.args));
+    bundler.on('update', rebundle);
+    bundler.on('log', util.log.bind(util));
+    function rebundle() {
+        return bundler.bundle()
+            .on('error', util.log)
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(gulp.dest('./src'))
+    }
+
+    return rebundle();
+}
+
+// Standard handler
+function standardHandler(err) {
+    // Notification
+    notifier.notify({message: 'Error: ' + err.message});
+    // Log to console
+    util.log(util.colors.red('Error'), err.message);
+}
+
+// Handler for browserify
+function browserifyHandler(err) {
+    standardHandler(err);
+}
+gulp.task('test', function () {
+
+});
+gulp.task('build', ['browserify', 'inlinesource']);
+gulp.task('watch', ['watchify']);
