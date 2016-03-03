8b5fef452aa40627d3ffb76b36e0ca6f9230e35f
lasconic <lasconic@gmail.com>
2016-03-03 15:25:43 +0400

    deploy everything from dist

--- build/travis-upload-artifacts.sh
+++ build/travis-upload-artifacts.sh
@@ -2,8 +2,8 @@
 
 export ARTIFACTS_TARGET_PATHS=beta
 
-export TRAVIS_BUILD_DIR=dist
-export ARTIFACTS_PATHS=index.html:img/
+export TRAVIS_BUILD_DIR=
+export ARTIFACTS_PATHS=dist/
 export ARTIFACTS_PERMISSIONS=public-read
 
 artifacts upload
