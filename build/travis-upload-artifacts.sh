6e04c1f91ccdab6349b3fc53e1e730608776cd94
Frank Baele <frank@localhost.localdomain>
2016-03-03 11:57:30 +0100

    add wildcard for images

--- build/travis-upload-artifacts.sh
+++ build/travis-upload-artifacts.sh
@@ -3,7 +3,7 @@
 export ARTIFACTS_TARGET_PATHS=beta
 
 export TRAVIS_BUILD_DIR=dist
-export ARTIFACTS_PATHS=index.html
+export ARTIFACTS_PATHS=index.html:img/*
 export ARTIFACTS_PERMISSIONS=public-read
 
 artifacts upload
