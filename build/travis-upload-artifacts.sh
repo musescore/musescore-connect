46421b381267b37e0de5b846ec2b1758c0624581
lasconic <lasconic@gmail.com>
2016-03-15 16:21:46 +0400

    upload to / when tagged, to beta/ when not tagged

--- build/travis-upload-artifacts.sh
+++ build/travis-upload-artifacts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export ARTIFACTS_TARGET_PATHS=beta
+if [ ${TRAVIS_TAG} == '' ]; then
+    export ARTIFACTS_TARGET_PATHS=beta
+else
+    export ARTIFACTS_TARGET_PATHS=
 
 export TRAVIS_BUILD_DIR=
 export ARTIFACTS_PATHS=dist/
