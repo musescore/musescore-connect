7a089fd7370460bb0a3336415c0027453a123ba5
lasconic <lasconic@gmail.com>
2016-06-21 11:49:16 +0200

    delete translations.json if it's the same than the one online

--- build/travis-upload-artifacts.sh
+++ build/travis-upload-artifacts.sh
@@ -6,6 +6,15 @@ else
     export ARTIFACTS_TARGET_PATHS=beta
 fi
 
+# delete translations.json if it's the same than the one online
+wget https://s3.amazonaws.com/connect2.musescore.com/translations.json
+diff -q translations.json dist/translations.json > /dev/null
+if [ $? == 0 ]
+then
+  rm dist/translations.json
+fi
+rm translations.json
+
 export TRAVIS_BUILD_DIR=
 export ARTIFACTS_PATHS=dist/
 export ARTIFACTS_PERMISSIONS=public-read
